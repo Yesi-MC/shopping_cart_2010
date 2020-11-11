1c4ad71aca0494562ab3e53dfdf838f76ee76940
Yesi Meza <yesi.meza10@gmail.com>
2020-11-11 09:46:51 -0700

    created a vendors can sell test and method

--- lib/market.rb
+++ lib/market.rb
@@ -14,6 +14,18 @@ class Market
   def vendor_names
     @vendors.map do |vendor|
       vendor.name
-    end 
+    end
+  end
+
+  def vendors_that_sell(items)
+    sell =[]
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item|
+        if items == item.first
+          sell << vendor
+        end
+      end
+    end
+    sell  
   end
 end
